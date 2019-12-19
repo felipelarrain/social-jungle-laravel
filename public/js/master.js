84bb6d1d116e2e9d592a3db84324b9feb24c34a4
felipelarraindangel@gmail.com <felipelarraindangeli@gmail.com>
2019-12-19 17:29:42 -0300

    Cambios feli

--- public/js/master.js
+++ public/js/master.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', function(){
     var temasActuales
 
-    var cargarTema function(){
+
 
       let captura = document.querySelector('.captura')
       console.log('.captura')
       //para mandarlo a la bd usar un fetch
-    }
+
 });
