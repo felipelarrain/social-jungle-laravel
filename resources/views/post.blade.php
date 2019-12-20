91641d588c4b0c68d215923ed172157737db4d5b
nchimales <n.chimales@gmail.com>
2019-12-20 00:47:35 -0300

    se puede publicar

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -28,8 +28,10 @@
                   </div>
                   <div class="form-group">
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFileLang" lang="es" >
+                        
+                        <input type="file" class="custom-file-input" id="imagen" lang="es" name="imagen">
                         <label class="custom-file-label" for="customFileLang">Seleccionar Archivo</label>
+                        
                       </div>
                   </div>
 
@@ -54,19 +56,26 @@
         </div>-->
         <div class="posteos">
             <article class="post">
+                
+                    
+                
                 <div class="persona">
                     <img src="img/profile.png" alt=""  > <!-- imagen perfil -->
                     <h6>NOMBRE APELLIDO</h6>
                 </div>
-                <div class="informe">
-                    <h6 >DEPORTES</h6>
-                    <h3 >Titulo Nota</h3>
+                @foreach ($posts as $post)
+                <div class="informe" >
+                    <h6>{{$post->temas_id}}</h6>
+                    <h3 >{{$post->titulo}}</h3>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis numquam repudiandae voluptatibus consequuntur culpa repellendus, laborum quibusdam laboriosam, maxime ab, rem adipisci commodi tempora perspiciatis aperiam consequatur amet. Deserunt accusamus officiis voluptatum dolore, sit eius repellendus, perferendis, eos eaque debitis adipisci corporis illo dolorem suscipit culpa non corrupti nulla.
+                        {{$post->mensaje}}
                     </p>
-                    <img src="img/imagen.jpg" alt=""> <!-- imagen publicacion (chequear para que haya un tama;o maximo de imagen y que se adapte a eso, ) -->
+                    <img src="{{$post->imagen}}" alt=""> <!-- imagen publicacion (chequear para que haya un tama;o maximo de imagen y que se adapte a eso, ) -->
 
                 </div>
+
+
+                @endforeach
             </article>
             <hr>
             <article class="post">
@@ -78,7 +87,11 @@
                     <h6 >FUTBOL</h6>
                     <h3 >Titulo Nota</h3>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis numquam repudiandae voluptatibus consequuntur culpa repellendus, laborum quibusdam laboriosam, maxime ab, rem adipisci commodi tempora perspiciatis aperiam consequatur amet. Deserunt accusamus officiis voluptatum dolore, sit eius repellendus, perferendis, eos eaque debitis adipisci corporis illo dolorem suscipit culpa non corrupti nulla.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis numquam repudiandae voluptatibus consequuntur culpa.
+                        
+                        repellendus, laborum quibusdam laboriosam, maxime ab, rem adipisci commodi tempora perspiciatis aperiam consequatur amet. 
+                        
+                        Deserunt accusamus officiis voluptatum dolore, sit eius repellendus, perferendis, eos eaque debitis adipisci corporis illo dolorem suscipit culpa non corrupti nulla.
                     </p>
                     <div class="imagen">
                         <img src="img/3.jpg" alt=""> <!-- imagen publicacion (chequear para que haya un tama;o maximo de imagen y que se adapte a eso, ) -->
