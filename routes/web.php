8c11924c1dab5e225440441d0fdfd033dec13b70
nchimales <n.chimales@gmail.com>
2019-12-20 11:37:04 -0300

    rutas arregladas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,20 +9,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function(){
   return view('inicio');
 });
 
-//Ruta para configuracion
 
+//Ruta para configuracion
 Route::get('/configuracion', function(){
   return view('configuracion');})->middleware('auth');
+
 
 Route::get('configuracion', 'UserController@config');
 
 
 Route::post('configuracion/update','UserController@update')->name('config.update')->middleware(['auth','password.confirm']);
+
 
 //Rutas Perfil
 Route::get('/perfil', function(){
@@ -32,18 +32,22 @@ Route::get('/perfil', function(){
 
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/notificaciones ', function () {
     return view('notificaciones');
 })-> middleware('auth');
 
 
 //Ruta que tiene que ver con los temas
-
 Route::get('/temasreg', 'TemasController@index')->middleware('auth');
 
 Route::get('/post', 'PostController@index')->name('post')->middleware('auth');
 
-Route::get('/temasreg/{id}', 'TemasController@store');
+Route::post('/guardarPost', 'PostController@save');
+
+Route::get('/publicacion/{id}','PostController@show')->middleware('auth');
+
+Route::get('/temas/{id}', 'TemasController@temas', function () {
+  //return view('TemasController@temas');
+  return view(dd($id));
+ });
